e404663d0f4e58b4655f607def9feb5cb04512bd
davidsr999 <sanzruiz999@gmail.com>
2022-09-20 09:08:49 +0200

    last version

--- src/app/components/proyectos/proyectos.component.ts
+++ src/app/components/proyectos/proyectos.component.ts
@@ -8,19 +8,35 @@ import {
   // ...
 } from '@angular/animations';
 
+
+
 @Component({
   selector: 'app-proyectos',
   templateUrl: './proyectos.component.html',
   styleUrls: ['./proyectos.component.css'],
+  animations: [
+    trigger('toggle', [
+      state('in', style({opacity: 1})),
+      state('out', style({opacity: 0})),
+      transition('* => *', [
+        animate('1s ease')
+      ])
+    ])
+  ]
   
 })
 export class ProyectosComponent implements OnInit {
 
-  
+  istate: boolean = true;
   constructor() { }
 
   ngOnInit(): void {
     
+  }
+
+  toggle() {
+    console.log(this.istate);
+    this.istate = !this.istate;
   }
 
   
